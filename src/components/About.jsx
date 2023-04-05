ad853d930ec324b1144b4b2aca1cb49caf04ace8
Emmanuel J. Marcano Perez <emmanueljulian.marcano@gmail.com>
2023-04-05 13:40:51 +0200

    added animation on scroll for about section. it works now

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect, useRef, useCallback } from "react"
+import {useInView} from "react-intersection-observer"
 
 import alphabet from '../assets/alphabet.avif'
 import graduation from '../assets/diploma.png'
@@ -31,45 +32,9 @@ import certification from '../assets/certification.png'
 
 function About(){
 
-    let myRef = useRef()
-    let [myElementIsVisible, setMyElementIsVisible] = useState()
-
-    var options = {
-        root: myRef,
-        rootMargin: '500px',
-        threshold: 0
-    }
-
-    // if(entry.isIntersecting){
-    //     myRef.current.classList.add('appear')
-    // }
-
-    // else{
-    //     myRef.current.classList.remove('appear')
-        
-    // }
-
-   
-
-
-    // useEffect(function(){
-
-
-    //     let observer = new IntersectionObserver(function(entries){
-    //         let entry = entries[0]
-    //         console.log('entry', entry)
-          
-    //         setMyElementIsVisible(entry.isIntersecting)
-    //     })
-    //     observer.observe(myRef.current, options)
-
-    //     console.log( 'iin',myElementIsVisible)
-    //     console.log(myRef.current)
-
-    // },[])
-
-
-   
+    let {ref: diplomaRef, inView: diplomaIsVisible} = useInView()
+    let {ref: graphRef, inView: graphIsVisible} = useInView()
+    let {ref: certRef, inView: certIsVisible} = useInView()
 
     return (
       
@@ -131,7 +96,7 @@ function About(){
                </div>
 
                 <div className="skills-graph">
-                    <div ref={myRef} className="skills-graphic hidden-el">
+                    <div ref={graphRef} className={`skills-graphic ${graphIsVisible ? "appear": ""}`}>
                         <span className="front">Front</span>
                         <span className="back">Back</span>
                         <span className="inner-circle-1"></span>
@@ -142,20 +107,22 @@ function About(){
             </div>
 
 
-            <div className="developer-journey">
+            <div  className="developer-journey">
                 <div className="section-header lines">Timeline of my software development journey</div>
                 <div className="content-container grid">
-                    <div className="journey-text">
+                    <div  className="journey-text">
                         <h1>Computer Sciente Graduate</h1>
-                        <p>Graduated high honors with a Bachelor's Degree in Computer Science in the Summer of 2022. 
+                        <p >Graduated high honors with a Bachelor's Degree in Computer Science in the Summer of 2022. 
                             Took key coursework such as Data Structures & Algorithms, Operating Systems, 
-                            Systems Design and Networking
+                            Systems Design and Networking. Diploma is visible {`${diplomaIsVisible ? 'yes': 'no'}`}
                         </p>
+
                         <a target="_blank" className="btn" href="https://uagm.edu/es/oferta-academica/programas/ciencias-con-concentracion-en-ciencias-de-computos"> View Curriculum</a>
                     </div>
 
                     <div className="journey-image">
-                        <img className="grad" src={graduation} alt="" />
+                        <img ref={diplomaRef}  className={`grad ${diplomaIsVisible ? "appear": ""}`} src={graduation} alt="" />
+                
                     </div>
                 </div>
             </div>
@@ -170,13 +137,13 @@ function About(){
                         <p>After graduation i concluded that i wanted to specialize in Front-End Development. 
                             As i also wanted to learn React along the way, i decided to take a course taught by Meta engineers 
                             that covers everything there is to learn about Front-End Development 
-                            including React
+                            including React  Cert is visible {`${certIsVisible ? 'yes': 'no'}`}
                         </p>
                         <a target="_blank" className="btn" href="https://www.coursera.org/account/accomplishments/professional-cert/PQSHJAE7PV8N">View Certificate</a>
                     </div>
 
                     <div className="journey-image order2">
-                       <img className="cert" src={certification} alt="" />
+                       <img ref={certRef} className={`cert ${certIsVisible ? "appear": ""}`} src={certification} alt="" />
                     </div>  
                 </div>
             </div>
