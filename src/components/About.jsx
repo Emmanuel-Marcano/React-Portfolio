1fe606591b79f9d7c6c58fe1d28a4a49e2bd6e08
Emmanuel J. Marcano Perez <emmanueljulian.marcano@gmail.com>
2023-04-05 14:00:27 +0200

    removed on scroll animation from graph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,8 @@ import alphabet from '../assets/alphabet.avif'
 import graduation from '../assets/diploma.png'
 import certification from '../assets/certification.png'
 
+let visibleDip;
+
 
 
 // let observer = new IntersectionObserver((entries) => {
@@ -33,7 +35,7 @@ import certification from '../assets/certification.png'
 function About(){
 
     let {ref: diplomaRef, inView: diplomaIsVisible} = useInView()
-    let {ref: graphRef, inView: graphIsVisible} = useInView()
+   
     let {ref: certRef, inView: certIsVisible} = useInView()
 
     return (
@@ -96,7 +98,7 @@ function About(){
                </div>
 
                 <div className="skills-graph">
-                    <div ref={graphRef} className={`skills-graphic ${graphIsVisible ? "appear": ""}`}>
+                    <div  className="skills-graphic">
                         <span className="front">Front</span>
                         <span className="back">Back</span>
                         <span className="inner-circle-1"></span>
@@ -114,15 +116,14 @@ function About(){
                         <h1>Computer Sciente Graduate</h1>
                         <p >Graduated high honors with a Bachelor's Degree in Computer Science in the Summer of 2022. 
                             Took key coursework such as Data Structures & Algorithms, Operating Systems, 
-                            Systems Design and Networking. Diploma is visible {`${diplomaIsVisible ? 'yes': 'no'}`}
+                            Systems Design and Networking.
                         </p>
 
                         <a target="_blank" className="btn" href="https://uagm.edu/es/oferta-academica/programas/ciencias-con-concentracion-en-ciencias-de-computos"> View Curriculum</a>
                     </div>
 
                     <div className="journey-image">
-                        <img ref={diplomaRef}  className={`grad ${diplomaIsVisible ? "appear": ""}`} src={graduation} alt="" />
-                
+                        <img ref={diplomaRef}  className={`grad ${diplomaIsVisible && "appear"}`} src={graduation} alt="" />
                     </div>
                 </div>
             </div>
@@ -137,13 +138,13 @@ function About(){
                         <p>After graduation i concluded that i wanted to specialize in Front-End Development. 
                             As i also wanted to learn React along the way, i decided to take a course taught by Meta engineers 
                             that covers everything there is to learn about Front-End Development 
-                            including React  Cert is visible {`${certIsVisible ? 'yes': 'no'}`}
+                            including React
                         </p>
                         <a target="_blank" className="btn" href="https://www.coursera.org/account/accomplishments/professional-cert/PQSHJAE7PV8N">View Certificate</a>
                     </div>
 
                     <div className="journey-image order2">
-                       <img ref={certRef} className={`cert ${certIsVisible ? "appear": ""}`} src={certification} alt="" />
+                       <img ref={certRef} className={`cert ${certIsVisible && "appear"}`} src={certification} alt="" />
                     </div>  
                 </div>
             </div>
