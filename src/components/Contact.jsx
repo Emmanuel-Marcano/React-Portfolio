e5787b10005719178e17b5656513ef60805135c6
Emmanuel J. Marcano Perez <emmanueljulian.marcano@gmail.com>
2023-04-13 23:00:19 +0200

    trying a new transition for contact cards

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,15 +13,16 @@ function Contact(){
                        Thank you for checking out my portfolio. 
                        I look forward to hearing from you! </p>
 
-                       <a className="btn" href="">Button</a>
+                       {/* <a className="btn" href="">Button</a> */}
                 </div>
                 <div className="contact-cards">
 
                     <div className="card linkedin-card">
                         <div className="card-curve">
-                            <a className="contact-icons contact-linkedin" href=""> <i className="fa-brands fa-linkedin-in fa-2x"></i></a>
+                            <span>CONTACT</span>
+                            <a target="_blank" className="contact-icons contact-linkedin" href="https://www.linkedin.com/in/emmanuel-marcano-a7a847246/"> <i className="fa-brands fa-linkedin fa-2x"></i></a>
                             <a className="contact-icons contact-email " href=""> <i className="fa-solid fa-envelope fa-2x"></i></a>
-                            <a className="contact-icons contact-github" href=""> <i className="fa-brands fa-github fa-2x"></i></a>
+                            <a target="_blank" className="contact-icons contact-github" href="https://github.com/Emmanuel-Marcano"> <i className="fa-brands fa-github fa-2x"></i></a>
                         </div>
                     </div>
 
