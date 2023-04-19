f1ff2e24a76fea430050cf33dd1c28fbe073fee1
Emmanuel J. Marcano Perez <emmanueljulian.marcano@gmail.com>
2023-04-19 13:13:14 +0200

    added a curtain transition for home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,21 +5,23 @@ function Home(){
     return (
         <div className="homepage">
 
-            <h1>
-                <RoughNotation type="underline" strokeWidth={2} show={true}>
-                Hi, my name is Emmanuel
-                </RoughNotation>
-                </h1>
-                <p className="static-text">I'm an Amsterdam based</p>
-                <p className="static-text">Front-End Developer with</p>
-                <div className="flex">
-                    <p className="static-text">experience in</p>
-                    <ul className="dynamic-text accent">
-                        <li> <span>HTML</span> </li>
-                        <li> <span>CSS</span> </li>
-                        <li><span>JavaScript</span> </li>
-                        <li> <span>React</span> </li>
-                    </ul>
+            <div className="light-half">
+
+                    <h1>Hi, my name is Emmanuel</h1>
+                </div>
+
+                <div className="dark-half">
+                    <p className="static-text">I'm an Amsterdam based</p>
+                    <p className="static-text">Front-End Developer with</p>
+                    <div className="flex">
+                        <p className="static-text">experience in</p>
+                        <ul className="dynamic-text accent">
+                            <li> <span>HTML</span> </li>
+                            <li> <span>CSS</span> </li>
+                            <li><span>JavaScript</span> </li>
+                            <li> <span>React</span> </li>
+                        </ul>
+                    </div>
                 </div>
         </div>
     )
